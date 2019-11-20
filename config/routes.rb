16a9a402da14720fffb081a262d47e229489a176
jeancloei <jeancloei@gmail.com>
2019-11-19 19:47:23 -0500

    log in funtionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users
   get 'hello_world', to: 'hello_world#index'
+  root 'items#index'
   resources :items, only: [:index]
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
