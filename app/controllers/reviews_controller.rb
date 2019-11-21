ad369876b16c9e2835cb766a19ca4edcaa158ae9
Gustavo Abdelnour <gurabdelnour@gmail.com>
2019-11-20 21:44:43 -0500

    cleanup

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -3,14 +3,11 @@ class ReviewsController < ApplicationController
     @item = Item.find(params[:item_id])
     @review = @item.reviews.new(review_params)
     @review.user = current_user
-    if @review.save
-      redirect_to @item
-    end
-
+    redirect_to @item if review.save
   end
 
   private 
     def review_params 
-      params.require(:review).permit(:comments, :rating, :id)
+      params.require(:review).permit(:comments, :rating)
     end 
 end
