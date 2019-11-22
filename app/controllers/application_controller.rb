af5992b8058fe3697a93b90ce383a8d3b3434657
cristhianFG93 <cris.f.g93@hotmail.com>
2019-11-21 19:41:45 -0500

    create admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,19 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :set_profile
+
+  def set_profile
+    @profile = current_user&.profile || Profile.find_by(id: session[:profile_id]) || Profile.new
+  end
+
+  protected
+
+  def after_sign_in_path_for(resource)
+    if resource.admin
+      admin_root_url
+    else
+      root_url
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,10 @@ Rails.application.routes.draw do
   root 'items#index'
   resources :items, only: [:index, :show] do 
     resources :reviews, only: [:create]
+end
+  namespace :admin do
+    root 'users#index'
+    resources :users, only: [:index]
   end
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
