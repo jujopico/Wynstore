302cd4311e52afd1cf7c0e808d9d438fd645a49e
Lazaro <lquin008@gmail.com>
2019-11-22 20:31:47 -0500

    MVC for creating cart items

---
+++ app/controllers/cart_items_controller.rb
@@ -0,0 +1,18 @@
+class CartItemsController < ApplicationController
+  def create
+    
+    @item = Item.find_by_id(params[:item_id])
+    @size = Size.find_by(size_name:params[:commit])
+    @item_size = ItemSize.find_by(item:@item, size:@size)
+    @cart = current_user.cart
+    
+    @cart_item = CartItem.create(cart:@cart, item_size:@item_size)
+
+    if @cart_item.save
+      puts "Awesome!"
+    else
+      puts @cart_item.errors.full_messages.to_sentence.capitalize
+    end
+  end
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
     resources :reviews, only: [:create]
 end
 resources :carts, only: [:show]
+resources :cart_items, only: [:create]
   namespace :admin do
     root 'users#index'
     resources :users, only: [:index]
