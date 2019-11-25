753540d77a5dfd5466ccc1e72302fecaba2932f2
Gustavo Abdelnour <gurabdelnour@gmail.com>
2019-11-25 15:11:29 -0500

    changed current_user to current_or_guest_user (as per devise-guest documentation) where it makes sense

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,16 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
+  # before_action :authenticate_user!
   before_action :set_profile
   before_action :set_cart
 
   def set_profile
-    @profile = current_user&.profile || Profile.find_by(id: session[:profile_id]) || Profile.new
+    @profile = current_or_guest_user&.profile || Profile.find_by(id: session[:profile_id]) || Profile.new
   end
 
   protected
 
   def set_cart
-    @cart = Cart.find_or_create_by(user:current_user)
+    @cart = Cart.find_or_create_by(user:current_or_guest_user)
   end
 
   def after_sign_in_path_for(resource)
