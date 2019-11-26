fa16777f294b1298e9e19d124df70ce22817c7af
Lazaro <lquin008@gmail.com>
2019-11-25 19:46:47 -0500

    schema changed to refelct phone change

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_25_193150) do
+ActiveRecord::Schema.define(version: 2019_11_26_003122) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -66,7 +66,7 @@ ActiveRecord::Schema.define(version: 2019_11_25_193150) do
   create_table "profiles", force: :cascade do |t|
     t.string "name"
     t.string "shipping_address"
-    t.integer "phone_number"
+    t.string "phone_number"
     t.string "email"
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
