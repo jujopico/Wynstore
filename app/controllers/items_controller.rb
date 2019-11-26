035c0cc8199b37a0cbeef6d3e34654d32cae675b
jujopico <sujunqt@gmail.com>
2019-11-26 18:29:08 -0500

    saving work in progress

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,7 +7,11 @@ class ItemsController < ApplicationController
   def show 
     @item = Item.find(params[:id])
     @sizes = @item.sizes.map { |size| size.size_name }
-    @reviews = Review.where(item_id: @item.id)
-  end
+    @reviews = Review.where(item_id: @item.id) #comments
 
+    @number = Review.find(params[:id]).rating
+    @stars = ('<i class="fas fa-star" aria-hidden="true"></i>'* @number + '<i class="far fa-star" aria-hidden="true"></i>'* (5 - @number)).html_safe
+
+  end
 end
+  
