46d5a008cceca188386b18bfab6691e949faca18
cristhianFG93 <cris.f.g93@hotmail.com>
2019-11-26 20:54:15 -0500

    admin-functionality-view

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -2,4 +2,25 @@ class Admin::UsersController < Admin::BaseController
     def index
         @users = User.all
     end
+
+    def show 
+        @user = User.find(params[:id])
+        @orders = @user.orders.map do |order|
+            {
+                id: order.id,
+                total: order.total,
+                items: order.cart.items.map do |item| 
+                    {
+                        item_name: item.item_name,
+                        item_quantity: item.quantity,
+                        item_sizes: item.item_sizes,
+
+
+                        
+                        
+                    }
+                end
+            }
+        end
+    end 
 end
