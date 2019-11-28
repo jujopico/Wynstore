a94be4e54f8bf5c0d06d6166416b90182047cb03
jujopico <sujunqt@gmail.com>
2019-11-27 21:25:57 -0500

    add average rating stars

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,5 +3,9 @@ class Item < ApplicationRecord
   has_many :cart_item, dependent: :destroy
   has_many :item_sizes
   has_many :sizes, through: :item_sizes
+
+  def average_rating 
+    reviews.reduce(0) { |acc, review| acc + review.rating } / reviews.length if !reviews.empty?
+  end 
 end
 
