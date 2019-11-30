4260a33a4e0383c83336a70ca079df3547199899
jujopico <sujunqt@gmail.com>
2019-11-30 15:43:21 -0500

    add poses model

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,9 +3,10 @@ class Item < ApplicationRecord
   has_many :cart_item, dependent: :destroy
   has_many :item_sizes
   has_many :sizes, through: :item_sizes
+  has_many :item_images
 
-  def average_rating 
-    (reviews.reduce(0) { |acc, review| acc + review.rating } / reviews.length).round if !reviews.empty?
-  end 
+  def average_rating
+    return 0 if reviews.empty?
+    (reviews.where.not(rating: nil).reduce(0) { |acc, review| acc + review.rating } / reviews.length).round
+  end
 end
-
