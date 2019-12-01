ea1a8a53f8e5116c253506bba0bcd974a13271d2
Jennifer Jordan <jbaldwin4@hotmail.com>
2019-12-01 12:23:33 -0500

    created table for admin orders page

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,5 +1,14 @@
 class Admin::OrdersController < Admin::BaseController
   def index
+      @users = User.all
       @orders = Order.all
   end
+
+  def show
+    @order = Order.find(params[:id])
+    @cart_items = @order.cart.cart_items
+    @user = @order.user
+  end
+
+
 end
